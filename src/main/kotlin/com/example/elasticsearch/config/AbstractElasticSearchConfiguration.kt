084621f8c57544a6b9bd668202e15cfcf86523c6
BrianDYKim <18k7102dy@naver.com>
2022-10-31 23:29:22 +0900

    Member Repository 정의

--- src/main/kotlin/com/example/elasticsearch/config/AbstractElasticSearchConfiguration.kt
+++ src/main/kotlin/com/example/elasticsearch/config/AbstractElasticSearchConfiguration.kt
@@ -1,8 +1,25 @@
 package com.example.elasticsearch.config
 
+import org.elasticsearch.client.RestHighLevelClient
+import org.springframework.context.annotation.Bean
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter
+
 /**
  * @author Doyeop Kim
  * @since 2022/10/31
  */
 abstract class AbstractElasticSearchConfiguration {
+    @Bean
+    abstract fun elasticsearchClient(): RestHighLevelClient
+
+    @Bean
+    fun elasticsearchOperations(
+        elasticsearchConverter: ElasticsearchConverter,
+        elasticsearchClient: RestHighLevelClient
+    ): ElasticsearchOperations {
+
+        return ElasticsearchRestTemplate(elasticsearchClient, elasticsearchConverter)
+    }
 }
---
+++ src/main/kotlin/com/example/elasticsearch/entity/Status.kt
@@ -0,0 +1,9 @@
+package com.example.elasticsearch.entity
+
+/**
+ * @author Doyeop Kim
+ * @since 2022/10/31
+ */
+enum class Status {
+    REGISTERED
+}
